299ec0d37500b3fcdcb9c12767e716efb5a632d5
AlaHariVital <100507619+AlaHariVital@users.noreply.github.com>
2023-08-06 15:01:40 +0530

    general commit

---
+++ JavaScript/ExecutionContextCheck/script.js
@@ -0,0 +1,12 @@
+const x = 10;
+const y = 5;
+function getSum(n1, n2) {
+  const sum = n1 + n2;
+  return sum;
+}
+
+const sum1 = getSum(x, y);
+const sum2 = getSum(4, 5);
+
+console.log(sum1);
+console.log(sum2);
---
+++ JavaScript/RevisonUntil7hrs/script.js
@@ -0,0 +1,14 @@
+//using typeof variable
+let num = '100';
+
+console.log(num, typeof num);
+
+num = +num;
+console.log(num, typeof num);
+
+const x = 5 + '5';
+console.log(x, typeof x);
+const y = 5 * '5';
+console.log(y, typeof y);
+
+const arr = [1, 2, 3, 4, 5, 6];
