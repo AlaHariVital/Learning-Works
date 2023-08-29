34938c30525e653744b30581ca2382752b19e286
AlaHariVital <100507619+AlaHariVital@users.noreply.github.com>
2023-08-29 18:52:07 +0530

    learning react

---
+++ meta front end dev/wrirting-test-with-jest/addFive.js
@@ -0,0 +1,5 @@
+function addFive(val) {
+  return val + 5;
+}
+//with the below code we can make a function globally acessable i.e  it can be used by other files in this project.
+module.exports = addFive;
